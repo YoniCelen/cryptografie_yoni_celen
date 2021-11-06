71460eef173d3cb977de2f68a9a96e0c4b5bf314
YoniCelen <yoni.celen@hotmail.com>
2021-11-06 16:48:03 +0100

    seems to work (get still exist as a testing tool)

--- solution/decrypt.js
+++ solution/decrypt.js
@@ -3,15 +3,19 @@ const _sodium = require('libsodium-wrappers')
 module.exports = {
      decryptChallenge : async function(jsonKey,jsonNonce,jsonChallenge) {
         await _sodium.ready
-        const key = _sodium.from_base64(jsonKey,_sodium.base64_variants.ORIGINAL)
-        const nonce = _sodium.from_base64(jsonNonce,_sodium.base64_variants.ORIGINAL)
-        const challenge = _sodium.from_base64(jsonChallenge,_sodium.base64_variants.ORIGINAL)
+        const key = _sodium.from_base64(jsonKey,_sodium.base64_variants.ORIGINAL);
+        const nonce = _sodium.from_base64(jsonNonce,_sodium.base64_variants.ORIGINAL);
+        const challenge = _sodium.from_base64(jsonChallenge,_sodium.base64_variants.ORIGINAL);
 
         const solution = _sodium.crypto_secretbox_open_easy(challenge,nonce,key);
 
         console.log(solution);
 
-        return solution;
+        const solutionEncoded = _sodium.to_base64(solution,_sodium.base64_variants.ORIGINAL);
+
+        console.log(solutionEncoded)
+
+        return solutionEncoded;
     }
 }
 
--- solution/request.js
+++ solution/request.js
@@ -1,35 +1,31 @@
-const axios = require('axios')
+const axios = require('axios');
+
 const link = "https://79vo67ipp9.execute-api.eu-west-1.amazonaws.com/Prod/decrypt/challenges"
 
+const post = async () => {
+    return await axios
+        .post(link)
+        .catch(error => {
+            console.error(error)
+        })
+}
+const get = async (kid) => {
+    return await axios
+        .get(link + '/' + kid)
+        .catch(error => {
+            console.error(error)
+        })
+}
+const del = async (kid, data) => {
+    await axios
+        .delete(link + '/' + kid, {data : {plaintext : data}})
+        .then(ret => console.log(ret.status))
+        .catch(error => {
+            console.error(error)
+        })
+}
+
 module.exports = {
-    post : function(){
-        axios
-            .post(link)
-            .then(res => {
-                //console.log(`statusCode: ${res.status}`)
-                //console.log(res)
-                return res.data;
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    },
-    get : function(kid){
-        axios
-            .get(link + '/' + kid)
-            .then(res => {
-                //console.log(`statusCode: ${res.status}`)
-                //console.log(res)
-                console.log(res.data)
-                return res.data;
-            })
-            .catch(error => {
-                console.error(error)
-            })
-
-    },
-    del : function (kid,solution){
-
-    }
+ post,get,del
 }
 
